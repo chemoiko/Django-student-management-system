8eed5ef9f870d9ec6a597b4f8fe4316adf654eb5
eli <elijahmurket@gmail.com>
2024-12-10 23:25:13 +0300

    added1

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -149,6 +149,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'main_app.CustomUser'
 AUTHENTICATION_BACKENDS = ['main_app.EmailBackend.EmailBackend']
